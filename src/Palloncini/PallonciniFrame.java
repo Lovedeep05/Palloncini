27a03cbf48e590abbd42e006144f57226797c7bd
Lovedeep05 <singhlovedeep613@gmail.com>
2023-02-26 19:42:54 +0100

    point system & bombBalloon

--- src/Palloncini/PallonciniFrame.java
+++ src/Palloncini/PallonciniFrame.java
@@ -8,9 +8,8 @@ import java.awt.event.*;
 import java.util.Random;
 
 public class PallonciniFrame extends JFrame {
-
-
-    JLabel redBalloon,blackBalloon,hotAirBalloon;
+    JLabel redBalloon,bombBalloon,hotAirBalloon,punteggioLabel;
+    int punteggio=0;
 
     private final int FRAME_WIDTH = 500;
     private final int FRAME_HEIGHT = 700;
@@ -25,9 +24,8 @@ public class PallonciniFrame extends JFrame {
             public void actionPerformed(ActionEvent evt) {
                 c=true;
                 moveSingleBalloon(redBalloon);
-                moveSingleBalloon(blackBalloon);
+                moveSingleBalloon(bombBalloon);
                 moveSingleBalloon(hotAirBalloon);
-
             }
 
         };
@@ -36,7 +34,7 @@ public class PallonciniFrame extends JFrame {
 
     private void moveSingleBalloon(JLabel balloon) {
         balloon.setLocation((balloon.getLocation().x), (int) (balloon.getLocation().y-difficulty));
-        if(balloon.getLocation().y<0){
+        if(balloon.getLocation().y<-50){
             balloon.setLocation((int)(Math.random() * ((FRAME_WIDTH) + 1)), FRAME_HEIGHT);
         }
 
@@ -49,6 +47,19 @@ public class PallonciniFrame extends JFrame {
                     difficulty+=0.2;
                     System.out.println(difficulty);
                     c=false;
+                    System.out.println(balloon.getName());
+                    switch (balloon.getName()){
+                        case "redBalloon":
+                            punteggio++;
+                            break;
+                        case "hotAirBalloon":
+                            punteggio+=3;
+                            break;
+                        case "bombBalloon":
+                            punteggio-=5;
+                            break;
+                    }
+                    punteggioLabel.setText("Punteggio: "+punteggio);
                 }
             }
         });
@@ -65,23 +76,29 @@ public class PallonciniFrame extends JFrame {
 
         mainPanel.setBackground(Color.green);
 
-
-
-
         redBalloon = new JLabel(new ImageIcon("redBalloon.png"));
         redBalloon.setBounds((int)(Math.random() * ((FRAME_WIDTH) + 1)),FRAME_HEIGHT,30,72);
+        redBalloon.setName("redBalloon");
 
-        blackBalloon = new JLabel(new ImageIcon("blackBalloon.png"));
-        blackBalloon.setBounds((int)(Math.random() * ((FRAME_WIDTH) + 1)),FRAME_HEIGHT,88,70);
+        bombBalloon = new JLabel(new ImageIcon("bombBalloon.png"));
+        bombBalloon.setBounds((int)(Math.random() * ((FRAME_WIDTH) + 1)),FRAME_HEIGHT,150,150);
+        bombBalloon.setName("bombBalloon");
 
         hotAirBalloon = new JLabel(new ImageIcon("hotAirBalloon.png"));
+        hotAirBalloon.setName("hotAirBalloon");
         hotAirBalloon.setBounds((int)(Math.random() * ((FRAME_WIDTH) + 1)),FRAME_HEIGHT,100,100);
 
 
 
+
+        punteggioLabel = new JLabel("Punteggio: "+punteggio);
+        punteggioLabel.setBounds(0,0,150,15);
+
         mainPanel.add(redBalloon);
-        mainPanel.add(blackBalloon);
+        mainPanel.add(bombBalloon);
         mainPanel.add(hotAirBalloon);
+
+        mainPanel.add(punteggioLabel);
 
         this.add(mainPanel);
         this.setVisible(true);
