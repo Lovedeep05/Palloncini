e0136bf7e55d06f76d2f0595278ea875c94e9983
Lovedeep05 <singhlovedeep613@gmail.com>
2023-02-26 16:46:01 +0100

    update2

--- src/Palloncini/PallonciniFrame.java
+++ src/Palloncini/PallonciniFrame.java
@@ -2,36 +2,53 @@ package Palloncini;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.util.Random;
 
 public class PallonciniFrame extends JFrame {
 
 
-    JLabel palloncino1,palloncino2;
+    JLabel redBallon,blackBallon;
+
+    private final int FRAME_WIDTH = 500;
+    private final int FRAME_HEIGHT = 700;
 
     public void moveBalloons()
     {
-
-        int delay = 30;
+        int difficulty = 0;
+        int delay = 60 - difficulty;
         ActionListener taskPerformer = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent evt) {
-                palloncino1.setLocation((palloncino1.getLocation().x), palloncino1.getLocation().y-5);
-                if(palloncino1.getLocation().y<0){
-                    palloncino1.setLocation(250, 500);
+                redBallon.setLocation((redBallon.getLocation().x), redBallon.getLocation().y-5);
+                if(redBallon.getLocation().y<0){
+                    redBallon.setLocation((int)(Math.random() * ((FRAME_WIDTH) + 1)), 500);
                 }
-                palloncino2.setLocation((palloncino2.getLocation().x), palloncino2.getLocation().y-5);
-                if(palloncino2.getLocation().y<0){
-                    palloncino2.setLocation(400, 500);
+
+                MouseListener MouseAdapter;
+                redBallon.addMouseListener(new MouseAdapter()
+                {
+                    public void mouseClicked(MouseEvent e)
+                    {
+                        redBallon.setLocation((int)(Math.random() * ((FRAME_WIDTH) + 1)), 500);
+                        difficulty++;
+                    }
+                });
+
+                blackBallon.setLocation((blackBallon.getLocation().x), blackBallon.getLocation().y-5);
+                if(blackBallon.getLocation().y<0){
+                    blackBallon.setLocation((int)(Math.random() * ((FRAME_WIDTH) + 1)), 500);
                 }
             }
         };
         new Timer(delay, taskPerformer).start();
     }
 
+
+
     public PallonciniFrame() {
-        this.setSize(500,700);
+
+        this.setSize(FRAME_WIDTH,FRAME_HEIGHT);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
 
@@ -43,18 +60,17 @@ public class PallonciniFrame extends JFrame {
         ImageIcon redBalloon = new ImageIcon("redBalloon.png");
         ImageIcon blackBalloon = new ImageIcon("blackBalloon.png");
 
-        palloncino1 = new JLabel(redBalloon);
-        palloncino1.setBounds(100,420,30,72);
+        redBallon = new JLabel(redBalloon);
+        redBallon.setBounds(100,FRAME_HEIGHT,30,72);
 
-        palloncino2 = new JLabel(blackBalloon);
-        palloncino2.setBounds(200,350,88,70);
+        blackBallon = new JLabel(blackBalloon);
+        blackBallon.setBounds(200,FRAME_HEIGHT,88,70);
 
         moveBalloons();
 
 
-
-        mainPanel.add(palloncino1);
-        mainPanel.add(palloncino2);
+        mainPanel.add(redBallon);
+        mainPanel.add(blackBallon);
 
         this.add(mainPanel);
         this.setVisible(true);
